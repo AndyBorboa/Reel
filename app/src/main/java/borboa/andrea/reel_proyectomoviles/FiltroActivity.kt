25df7f4cd2e6f7f74429cd048c87e053d1c1ac3f
Ricardo Yair Martinez Vazquez <98051181+yairmc@users.noreply.github.com>
2022-04-06 23:48:20 -0700

    Layout filtros en progreso

---
+++ app/src/main/java/borboa/andrea/reel_proyectomoviles/FiltroActivity.kt
@@ -0,0 +1,11 @@
+package borboa.andrea.reel_proyectomoviles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FiltroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtro)
+    }
+}
